bed4233c1d910fd42afda7bbedba0e742d713978
Meredith Jones <mj19003@gmail.com>
2020-12-04 18:16:55 -0500

    Friday night updates with readme and images

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# coding_quiz
-Week 4 Homework
+Meredith Jones: Javascript Coding Quiz
+==================================================
+This project was by far the most challening coding task that I have embarked on to date. 
+
+The function was to be a quiz which would time participents, track their score, as well as deduct five seconds from the timer for incorrect answers.
+
+I created 5 multiple choice answers on the subject of coding. I enjoyed creating the content of the questions. 
+
+It was the first time that I used Javascript to manipulate the DOM in order to create content and elements in the html dynamically. 
+It was also my first time using a "For Each" loop rather that the for loop that I have used in the past. An image of that function is included below for reference. 
+![An image of the section of project code which includes a for each loop ](assets/images/image2.png)
+
+I was focused almost entierly on the function of the page, so the styling defintiely leaves something to be desired as you will see in the screenshot below. 
+![An image of the initial screen containing the Coding Quiz](assets/images/image1.png)
+In an ideal world, I would certainly be presenting something with a more dynamic appearance. 
+
+Although the function of the quiz is fairly simple, I found all of the layers to create it to be extremely complex. I devoted many hours of work to creating the quiz, but sadly was unable to add many functions before my time to work on the project ran out. 
+
+I was happy to be able to get a countdown functioning, as well as increasing and storing the user score. Both of those functions presneted me with many hours of struggle to get them working.
+
+I know that this is far from my best work, but it reprents a lot of hard work. I can say for sure that I have learned a lot in the process of creating the quiz. 
--- script.js
+++ script.js
@@ -28,7 +28,8 @@
       question: "What does HTML stand for?",
       choices: ["How To Make lines", "HyperText Markup Language", "Hexagon Textbox Marker Lineup", "Helping Toolkit Makes Loops"],
       answer: "HyperText Markup Language"
-          }
+          },
+
 ]
 
 var questionIndex = 0;
@@ -55,10 +56,10 @@ var score = document.getElementById("correct");
 
 //Beginning the game:
 function btnClick(){
-    if (questionIndex>=questions.length) {
+    if (questionIndex>= questions.length) {
         // startButton.setAttribute("class", "hide")
-        quizArea.setAttribute("class", "hide");
-        timeDiv.setAttribute("class", "hide");
+        quizArea.setAttribute("class", "hide")
+        timeDiv.setAttribute("class", "hide")
         return;
     }
     console.log(this.value, questions[questionIndex].answer)
@@ -69,7 +70,8 @@ console.log(this.value, questions[questionIndex].answer)
         penalty()
         //penalize time- decduct 5 seconds
         questionIndex++
-        setNextQuestion();
+        setNextQuestion()
+        
         
         
        
@@ -118,6 +120,7 @@ function setNextQuestion(){
         choiceBtn.onclick = btnClick;
         choicesDiv.appendChild(choiceBtn);
         console.log(choiceBtn)
+        trackScore(numCorrect)
 
     })
 
@@ -126,24 +129,6 @@ function setNextQuestion(){
 
 
 
-// //Second set with question and answer options: 
-// function setNextQuestion(){
-//     console.log(questions[questionIndex]+1)
-//     var currentQuest = questions[questionIndex]+1
-//     var quest = document.getElementById("question");
-//     quest.textContent = currentQuest.question;
-//     //showQuestion(currentQuestionIndex)
-//     currentQuest.choices.forEach(function(choice, i){
-//         console.log(choice, i)
-//         var choiceBtn = document.createElement("button");
-//         choiceBtn.setAttribute("class", "choice");
-//         choiceBtn.setAttribute("value", choice)
-//         choiceBtn.textContent = choice;
-//         choiceBtn.onclick = btnClick;
-//         choicesDiv.appendChild(choiceBtn);
-//         console.log(choiceBtn)
-
-//     })
 
 // };
 // Setting up the timer on the HTML
@@ -177,198 +162,6 @@ function startGame(){
     timer = setInterval(tick, 1000)
     setNextQuestion();
 };
-
-
-
-
-//Creating the quiz variables
-//We will need a question prompt, 4 answer options, and what the correct answer is. 
-
-// questionEl.appendChild(questions);
-// questionEl.innerHTML="Helllooooo There"
-
-// var questions [
-//     { Prompt:""
-//       answer:""  
-//     }
-// ]
-// var highScore []
-
-
-//we will need a loop to go through the questions, and a way to track the user's responses.
-//NEED TO CREATE A LOOP TO ASK THE QUESTIONS 
-//NEED TO CREATE A WAY TO CHECK IF ANSWERS ARE TRUE OR FALSE 
-
-// for ( var i = 0; i < questions.length; i++) {
-//         var UserResponse 
-//         // Check if the answer is true or false
-//         if ((UserResponse === true && questions[i].a === "t") ||
-//         (UserResponse === false && questions[i].a === "f")) {
-//             console.log(this, this.value)
-//         // Increase score by 1 point
-//             score++;
-//             alert("Correct!");
-//             }
-//             else {
-//             alert("False!");
-//                 }
-        //continue to the next question and reduce the timer by 5 seconds
-
-// Show score at end of Quiz
-//alert("You got " + score + "/" + questions.length);
-
-//Card should appear with inputs for initials-> place that info & the score
-//into local storage 
-    
-//}
-
-
-// Advice from AskBCS: "you will want to access them by index as the quiz progresses, using a global 
-// questionIndex variable that increases after every question 
-// so that it can be used to access the next one.
-//  You would access it like this questions[questionIndex].question so, 
-//  depending on what the current value of questionIndex is, 
-//  will determine which question is selected
-
-
-
-
-
-//4:23
-//The first item in an array is at the zero index, so start out with questionIndex = 0
-
-
-//Put the quiz into an object
-
-//questions[questionIndex].choices[questionIndex] -> "answer" 
-
-
-//let allTheChoices = document.querySelector("#multipleChoices")
- //   allTheChoices.append('append() choices');
-
-//console.log(app.textContent);
-
-// var questions = getElementById("li").appendChild("question");
-
-// Creating buttons for answers:
-// var btn1 = document.createElement("BUTTON1");
-//Text to appear in the button, will need to pull from the array of "choices"
-// btn.innerHTML = ["choices"];
-// Append <button> to <body>                  
-// document.body.appendChild("btn1");
-//Should btn1 etc. be in quotes or not?
-
-// var btn2 = document.createElement("BUTTON2");
-//Text to appear in the button, will need to pull from the array of "choices"
-// btn.innerHTML = ["choices"];
-// Append <button> to <body>                  
-// document.body.appendChild(btn2);     
-
-////var btn3 = document.createElement("BUTTON3");
-//Text to appear in the button, will need to pull from the array of "choices"
-//btn.innerHTML = ["choices"];
-// Append <button> to <body>                  
-//document.body.appendChild(btn3);     
-
-//var btn4 = document.createElement("BUTTON4");
-//Text to appear in the button, will need to pull from the array of "choices"
-//btn.innerHTML = ["choices"];
-// Append <button> to <body>                  
-//document.body.appendChild(btn4);    
-
-//
-
-//container div is hidden, start button is visible 
-// attach an event listener (click ) to start button, which will trigger a function
-    // container div visible
-    // start button hidden
-    // set up and start timer (setInterval)
-    // call a function to set up questions (setQuestions)
-
-
-// set Questions, append variable question to html (the h2);**Need help doing this. 
-
-    // var currentQuestion = questions[questionIndex].question;
-    //     document.body.appendvar(h2); 
-    //append variable question to html (the h2);
-    // for loop over choices array
-        //- create an element (button)
-        // example    var button = document.createElement(button)
-        //- set atrribute (class name)
-        //- set attribute ( value, choice[i])
-        // button.textContent = choice[i]
-    //button.onlick = someFunction
-    //append button to html (choices)
-
-
-//someFunction
-    // is to compare users selection to correct answer, and then handle accordingly 
-    //console.log(this, this.value), 
-    //compare this.value to answer key to the current obj
-    //if this.value !== answer
-        //display some text "wrong"
-        // we need to take 5 away from the timer
-    //else (answered correctly)
-        // display "Correct!"
-
-    //questionIndex ++;
-    //if questionIndex === questions.length
-        //call the function endQuiz
-    //else
-        //call the function setQuestion
-
-  //Timer Script Here:
-  // Update the count down by every second that passes (except if an answer is incorrect)
-  
- //The code below for this timer was written by 
-// Adhithya Rajasekaran
-// Github user name adhithyan15
-
-// function tick() {
-//     // Shouldn't the code below make the timer appear on the html page? 
-//     var timer = document.getElementById("timer");
-//     var current_minutes = mins-1
-//     seconds--;
-//     timer.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
-//     if( seconds > 0 ) {
-//         setTimeout(tick, 1000);
-//     } else {
-//         if(mins > 1){
-//             countdown(mins-1);           
-//         }
-//     }
-// }
-
-
-//endQuiz
-    // stop our timer (clearInterval)
-    //hide quizArea
-    //display our final score 
-    
-//Overview of the assignment->
-//Start button with an event listener
-
-//on click, show first question
-
-//Your welcome visual needs to dissapear- add a Display none CSS property
-
-
-//add a start button in html
-
-//Create an array of objects to diplay the questions. Title, choices and answer
-
-//Create a div for the question document.createElemnent *Create a div placeholder
-//with id of quesitons, starting, have a hide, with your onlick, remove the hide class. 
-
-//Create an <h2> with the question title, then we need a div or an unordered list to house the buttons. 
-
-//queryselector get elementbyid to append the elements from the DOM
-
-//Grab the quesiton title DIV (getElementbyID)
-//Grab the optional aswers DIV
-
-//How do we access arrays? Question [0].title
-//Question [0].choices[0] Get to choices of "i" for each available option
 
 
 
